0bd814a3808780a62c1d78694e0c2bb2d33b3814
pauloscardoso <paulocardoso19@live.com>
2022-04-08 13:06:27 -0300

    Initial

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export const PostCard = ({ title, body, id, cover }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>
+        {title} {id}
+      </h1>
+      <p>{body}</p>
+    </div>
+  </div>
+);
